7f4c244fcbf6d9ff21dfd2248dc6abb1e0a115d6
Dillon SP <ca.dillon.sp@gmail.com>
2019-09-05 11:53:54 -0700

    Update directory paths

--- js/library/serviceworker.js
+++ js/library/serviceworker.js
@@ -2,8 +2,8 @@ var APP_PREFIX = 'SpecialStage'     // Identifier for this app (this needs to be
 var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
-  '/',                     // If you have separate JS/CSS files,
-  '/index.html'            // add path to those files here
+  '././',                     // If you have separate JS/CSS files,
+  '././index.html'            // add path to those files here
 ]
 
 // Respond with cached resources
