bd09949fe91f278a0940650eed6c670bea7ea24d
Dillon SP <ca.dillon.sp@gmail.com>
2019-10-25 04:53:31 -0700

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -13,10 +13,10 @@ self.addEventListener('fetch', function (e) {
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
+//        console.log('responding with cache : ' + e.request.url)
         return request
       } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
+//        console.log('file is not cached, fetching : ' + e.request.url)
         return fetch(e.request)
       }
 
