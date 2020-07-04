3b486ce0cb30e2abf3f872c3df95ce050f771cae
gloom <ca.dillon.sp@gmail.com>
2020-07-03 19:48:38 -0700

    UPDATE A_0001

--- serviceworker.js
+++ serviceworker.js
@@ -1,22 +1,22 @@
-var APP_PREFIX = 'SpecialStage_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = Date.now()
+var APP_PREFIX = 'SpecialStage'     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = BUILD
 VERSION.toString()              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
   '/',                     // If you have separate JS/CSS files,
   '/index.html'            // add path to those files here
 ]
-console.log( CACHE_NAME )
+
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
-  //console.log('fetch request : ' + e.request.url)
+  console.log('fetch request : ' + e.request.url)
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) { // if cache is available, respond with cache
-//        console.log('responding with cache : ' + e.request.url)
+        console.log('responding with cache : ' + e.request.url)
         return request
       } else {       // if there are no cache, try fetching request
-//        console.log('file is not cached, fetching : ' + e.request.url)
+        console.log('file is not cached, fetching : ' + e.request.url)
         return fetch(e.request)
       }
 
